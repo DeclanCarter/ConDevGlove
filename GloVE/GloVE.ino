7bd4bd99bc2edd25fab2a62b01a84ae72e109a6d
seamus <c00209242@itcarlow.ie>
2018-11-16 18:09:00 +0000

    Initial flashing

--- GloVE/GloVE.ino
+++ GloVE/GloVE.ino
@@ -1,8 +1,8 @@
 // arduino code for the gauntlet
-const int iLED = 13; // index finger LED
-const int mLED = 12; // middle finger LED
-const int rLED = 11; // ring finger LED
-const int lLED = 10; // little finger LED
+const int iLED = 12; // index finger LED
+const int mLED = 11; // middle finger LED
+const int rLED = 10; // ring finger LED
+const int lLED = 9; // little finger LED
 
 const int iSense = A2; // index finger sensor
 const int mSense = A3; // middle finger sensor
@@ -12,21 +12,24 @@ const int lSense = A5; // little finger sensor
 void setup() {
   for (int i = 0; i < 2; i++)
   {
-    for (int pinNo = 2; pinNo < 6; pinNo++) {
+    for (int pinNo = 9; pinNo < 13; pinNo++) {
       if (i == 0) {
         pinMode(pinNo, OUTPUT);
       }
       digitalWrite(pinNo, HIGH);
     }
-    delay(1);
-    for (int pinNo = 2; pinNo < 6; pinNo++) {
+    delay(500);
+    for (int pinNo = 9; pinNo < 13; pinNo++) {
       digitalWrite(pinNo, LOW);
     }
-    delay(1);
+    delay(500);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(iLED, HIGH);
+  digitalWrite(mLED, HIGH);
+  digitalWrite(rLED, HIGH);
 
 }
