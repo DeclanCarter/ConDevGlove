214119b31e9b4c76c43bf039a9f5b25f3a281e8e
seamus <c00209242@itcarlow.ie>
2018-11-16 17:22:07 +0000

    ard

--- GloVE/GloVE.ino
+++ GloVE/GloVE.ino
@@ -1,7 +1,29 @@
 // arduino code for the gauntlet
-void setup() {
-  // put your setup code here, to run once:
+const int iLED = 13; // index finger LED
+const int mLED = 12; // middle finger LED
+const int rLED = 11; // ring finger LED
+const int lLED = 10; // little finger LED
 
+const int iSense = A2; // index finger sensor
+const int mSense = A3; // middle finger sensor
+const int rSense = A4; // ring finger sensor
+const int lSense = A5; // little finger sensor
+
+void setup() {
+  for (int i = 0; i < 2; i++)
+  {
+    for (int pinNo = 2; pinNo < 6; pinNo++) {
+      if (i == 0) {
+        pinMode(pinNo, OUTPUT);
+      }
+      digitalWrite(pinNo, HIGH);
+    }
+    delay(1);
+    for (int pinNo = 2; pinNo < 6; pinNo++) {
+      digitalWrite(pinNo, LOW);
+    }
+    delay(1);
+  }
 }
 
 void loop() {
